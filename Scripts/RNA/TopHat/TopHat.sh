895cff7a7de118b0db312118b443b611b9d52911
Amanj1 <amanj5@msn.com>
2018-05-05 18:25:30 +0200

    new TopHat script problem with module tophat

--- Scripts/RNA/TopHat/TopHat.sh
+++ Scripts/RNA/TopHat/TopHat.sh
@@ -14,90 +14,59 @@ module load tophat/2.1.1
 
 # Commands
 
-sel2_SRR1719204.trim_1P.fastq  
-sel2_SRR1719204.trim_2P.fastq 
-sel2_SRR1719204.trim_1U.fastq 
-sel2_SRR1719204.trim_2U.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719013_P_thout sel2_SRR1719013_1P.fq sel2_SRR1719013_2P.fq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719013_U_thout sel2_SRR1719013_1U.fq sel2_SRR1719013_2U.fq
 
-sel2_SRR1719206.trim_1P.fastq
-sel2_SRR1719206.trim_2P.fastq  
-sel2_SRR1719206.trim_1U.fastq 
-sel2_SRR1719206.trim_2U.fastq 
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719204_trim_P_thout sel2_SRR1719204.trim_1P.fastq sel2_SRR1719204.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719204_trim_U_thout sel2_SRR1719204.trim_1U.fastq sel2_SRR1719204.trim_2U.fastq
 
-sel2_SRR1719207.trim_1P.fastq
-sel2_SRR1719207.trim_2P.fastq
-sel2_SRR1719207.trim_1U.fastq
-sel2_SRR1719207.trim_2U.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719206_trim_P_thout sel2_SRR1719206.trim_1P.fastq sel2_SRR1719206.trim_2P.fastq  
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719206_trim_U_thout sel2_SRR1719206.trim_1U.fastq sel2_SRR1719206.trim_2U.fastq 
 
-sel2_SRR1719208.trim_1P.fastq
-sel2_SRR1719208.trim_2P.fastq
-sel2_SRR1719208.trim_1U.fastq
-sel2_SRR1719208.trim_2U.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719207_trim_P_thout sel2_SRR1719207.trim_1P.fastq sel2_SRR1719207.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719207_trim_U_thout sel2_SRR1719207.trim_1U.fastq sel2_SRR1719207.trim_2U.fastq
 
-sel2_SRR1719209.trim_1P.fastq 
-sel2_SRR1719209.trim_2P.fastq 
-sel2_SRR1719209.trim_1U.fastq
-sel2_SRR1719209.trim_2U.fastq 
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719208_trim_P_thout sel2_SRR1719208.trim_1P.fastq sel2_SRR1719208.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719208_trim_U_thout sel2_SRR1719208.trim_1U.fastq sel2_SRR1719208.trim_2U.fastq
 
-sel2_SRR1719014.trim_1P.fastq 
-sel2_SRR1719014.trim_2P.fastq 
-sel2_SRR1719014.trim_1U.fastq
-sel2_SRR1719014.trim_2U.fastq 
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719209_trim_P_thout sel2_SRR1719209.trim_1P.fastq sel2_SRR1719209.trim_2P.fastq 
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719209_trim_U_thout sel2_SRR1719209.trim_1U.fastq sel2_SRR1719209.trim_2U.fastq 
 
-sel2_SRR1719015.trim_1P.fastq
-sel2_SRR1719015.trim_2P.fastq
-sel2_SRR1719015.trim_1U.fastq 
-sel2_SRR1719015.trim_2U.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719014_trim_P_thout sel2_SRR1719014.trim_1P.fastq sel2_SRR1719014.trim_2P.fastq 
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719014_trim_U_thout sel2_SRR1719014.trim_1U.fastq sel2_SRR1719014.trim_2U.fastq 
 
-sel2_SRR1719016.trim_1P.fastq
-sel2_SRR1719016.trim_2P.fastq
-sel2_SRR1719016.trim_1U.fastq
-sel2_SRR1719016.trim_2U.fastq 
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719015_trim_P_thout sel2_SRR1719015.trim_1P.fastq sel2_SRR1719015.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719015_trim_U_thout sel2_SRR1719015.trim_1U.fastq sel2_SRR1719015.trim_2U.fastq
 
-sel2_SRR1719017.trim_1P.fastq
-sel2_SRR1719017.trim_2P.fastq
-sel2_SRR1719017.trim_1U.fastq
-sel2_SRR1719017.trim_2U.fastq 
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719016_trim_P_thout sel2_SRR1719016.trim_1P.fastq sel2_SRR1719016.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719016_trim_U_thout sel2_SRR1719016.trim_1U.fastq sel2_SRR1719016.trim_2U.fastq 
 
-sel2_SRR1719018.trim_1P.fastq
-sel2_SRR1719018.trim_2P.fastq 
-sel2_SRR1719018.trim_1U.fastq
-sel2_SRR1719018.trim_2U.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719017_trim_P_thout sel2_SRR1719017.trim_1P.fastq sel2_SRR1719017.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719017_trim_U_thout sel2_SRR1719017.trim_1U.fastq sel2_SRR1719017.trim_2U.fastq 
 
-sel2_SRR1719211.trim_1P.fastq 
-sel2_SRR1719211.trim_2P.fastq
-sel2_SRR1719211.trim_1U.fastq  
-sel2_SRR1719211.trim_2U.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719018_trim_P_thout sel2_SRR1719018.trim_1P.fastq sel2_SRR1719018.trim_2P.fastq 
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719018_trim_U_thout sel2_SRR1719018.trim_1U.fastq sel2_SRR1719018.trim_2U.fastq
 
-sel2_SRR1719212.trim_1P.fastq
-sel2_SRR1719212.trim_2P.fastq
-sel2_SRR1719212.trim_1U.fastq
-sel2_SRR1719212.trim_2U.fastq 
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719211_trim_P_thout sel2_SRR1719211.trim_1P.fastq sel2_SRR1719211.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719211_trim_U_thout sel2_SRR1719211.trim_1U.fastq sel2_SRR1719211.trim_2U.fastq
 
-sel2_SRR1719213.trim_1P.fastq
-sel2_SRR1719213.trim_2P.fastq
-sel2_SRR1719213.trim_1U.fastq
-sel2_SRR1719213.trim_2U.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719212_trim_P_thout sel2_SRR1719212.trim_1P.fastq sel2_SRR1719212.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719212_trim_U_thout sel2_SRR1719212.trim_1U.fastq sel2_SRR1719212.trim_2U.fastq 
+
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719213_trim_P_thout sel2_SRR1719213.trim_1P.fastq sel2_SRR1719213.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719213_trim_U_thout sel2_SRR1719213.trim_1U.fastq sel2_SRR1719213.trim_2U.fastq
  
-sel2_SRR1719214.trim_1P.fastq
-sel2_SRR1719214.trim_2P.fastq
-sel2_SRR1719214.trim_1U.fastq
-sel2_SRR1719214.trim_2U.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719214_trim_P_thout sel2_SRR1719214.trim_1P.fastq sel2_SRR1719214.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719214_trim_U_thout sel2_SRR1719214.trim_1U.fastq sel2_SRR1719214.trim_2U.fastq
 
-sel2_SRR1719241.trim_1P.fastq   
-sel2_SRR1719241.trim_2P.fastq
-sel2_SRR1719241.trim_1U.fastq
-sel2_SRR1719241.trim_2U.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719241_trim_P_thout sel2_SRR1719241.trim_1P.fastq sel2_SRR1719241.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719241_trim_U_thout sel2_SRR1719241.trim_1U.fastq sel2_SRR1719241.trim_2U.fastq
 
-sel2_SRR1719242.trim_1P.fastq
-sel2_SRR1719242.trim_2P.fastq
-sel2_SRR1719242.trim_1U.fastq
-sel2_SRR1719242.trim_2U.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719242_trim_P_thout sel2_SRR1719242.trim_1P.fastq sel2_SRR1719242.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719242_trim_U_thout sel2_SRR1719242.trim_1U.fastq sel2_SRR1719242.trim_2U.fastq
 
-sel2_SRR1719266.trim_1P.fastq
-sel2_SRR1719266.trim_2P.fastq
-sel2_SRR1719266.trim_1U.fastq
-sel2_SRR1719266.trim_2U.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719266_trim_P_thout sel2_SRR1719266.trim_1P.fastq sel2_SRR1719266.trim_2P.fastq
+tophat -p 2 -r 200 -G sel2_NW_015504334.fna -o tophat_out_dir/sel2_SRR1719266_trim_U_thout sel2_SRR1719266.trim_1U.fastq sel2_SRR1719266.trim_2U.fastq
 
 
    
