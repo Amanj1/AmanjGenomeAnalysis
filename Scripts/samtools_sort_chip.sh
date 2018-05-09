dfa7085f71361961defed6474ff423dd18f42620
Amanj1 <amanj5@msn.com>
2018-05-09 17:20:59 +0200

    update sort script

--- Scripts/samtools_sort_chip.sh
+++ Scripts/samtools_sort_chip.sh
@@ -15,39 +15,39 @@ module load samtools/1.6
 # Commands
 #bwa index sel2_NW_015504334.fna
 
-samtools sort sam_to_bam/sel2_SRR1724092_aln-se.bam sorted_bam/sel2_SRR1724092_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724092_aln-se.bam -o sorted_bam/sel2_SRR1724092_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724095_aln-se.bam sorted_bam/sel2_SRR1724095_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724095_aln-se.bam -o sorted_bam/sel2_SRR1724095_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724098_aln-se.bam sorted_bam/sel2_SRR1724098_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724098_aln-se.bam -o sorted_bam/sel2_SRR1724098_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724101_aln-se.bam sorted_bam/sel2_SRR1724101_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724101_aln-se.bam -o sorted_bam/sel2_SRR1724101_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724104_aln-se.bam sorted_bam/sel2_SRR1724104_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724104_aln-se.bam -o sorted_bam/sel2_SRR1724104_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724108_aln-se.bam sorted_bam/sel2_SRR1724108_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724108_aln-se.bam -o sorted_bam/sel2_SRR1724108_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724093_aln-se.bam sorted_bam/sel2_SRR1724093_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724093_aln-se.bam -o sorted_bam/sel2_SRR1724093_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724096_aln-se.bam sorted_bam/sel2_SRR1724096_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724096_aln-se.bam -o sorted_bam/sel2_SRR1724096_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724099_aln-se.bam sorted_bam/sel2_SRR1724099_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724099_aln-se.bam -o sorted_bam/sel2_SRR1724099_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724102_aln-se.bam sorted_bam/sel2_SRR1724102_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724102_aln-se.bam -o sorted_bam/sel2_SRR1724102_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724105_aln-se.bam sorted_bam/sel2_SRR1724105_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724105_aln-se.bam -o sorted_bam/sel2_SRR1724105_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724109_aln-se.bam sorted_bam/sel2_SRR1724109_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724109_aln-se.bam -o sorted_bam/sel2_SRR1724109_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724094_aln-se.bam sorted_bam/sel2_SRR1724094_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724094_aln-se.bam -o sorted_bam/sel2_SRR1724094_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724097_aln-se.bam sorted_bam/sel2_SRR1724097_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724097_aln-se.bam -o sorted_bam/sel2_SRR1724097_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724100_aln-se.bam sorted_bam/sel2_SRR1724100_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724100_aln-se.bam -o sorted_bam/sel2_SRR1724100_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724103_aln-se.bam sorted_bam/sel2_SRR1724103_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724103_aln-se.bam -o sorted_bam/sel2_SRR1724103_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724107_aln-se.bam sorted_bam/sel2_SRR1724107_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724107_aln-se.bam -o sorted_bam/sel2_SRR1724107_aln-se-sorted.bam
 
-samtools sort sam_to_bam/sel2_SRR1724110_aln-se.bam sorted_bam/sel2_SRR1724110_aln-se-sorted.bam
+samtools sort sam_to_bam/sel2_SRR1724110_aln-se.bam -o sorted_bam/sel2_SRR1724110_aln-se-sorted.bam
 
